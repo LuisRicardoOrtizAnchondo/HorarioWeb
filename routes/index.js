6f748cef94acb8bbac7ea7b1b14f9df8e28cd655
Nuria Reyes <nuriatonantzin@gmail.com>
2017-11-04 21:19:13 -0700

    renamed .jade files into .pug files, added bootstrap and font awesome libraries

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/register', function(req, res) {
 router.post('/register', function(req, res, next) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
-          return res.render('register', { error : err.message });
+          return res.render('users/register', { error : err.message });
         }
 
         passport.authenticate('local')(req, res, function () {
@@ -35,7 +35,7 @@ router.post('/register', function(req, res, next) {
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+    res.render('users/login', { user : req.user });
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
@@ -50,8 +50,5 @@ router.get('/logout', function(req, res) {
 router.get('/ping', function(req, res){
     res.status(200).send("pong!");
 });
-
-module.exports = router;
-
 
 module.exports = router;
