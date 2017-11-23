702dd4879209cd1f4685a10939fec032afb660a6
Nuria Reyes <nuriatonantzin@gmail.com>
2017-11-23 00:07:12 -0700

    Added schedule table template

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -1,10 +1,8 @@
-$("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("active");
+$(function () {
+  $('#datetimepicker4').datetimepicker();
 });
 
 $(function () {
-  $('#datetimepicker4').datetimepicker();
   $('#dtclock1').datetimepicker({
       format: 'LT'
   });
